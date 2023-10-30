bca9a5217cf0dc396f1a9ad08a5096b290725d7e
GeroDLuffy <gerox2009@hotmail.com>
2023-10-30 18:24:34 -0300

    Inits, GUI_b, practica2

--- GUI/GUI_b.py
+++ GUI/GUI_b.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from A_scripts import practica2 as p2
+from ..A_scripts import master_ppsw as p2
 # Window
 ventana = Tk()
 
@@ -7,26 +7,20 @@ ventana.title('Password Manager')
 ventana.iconbitmap('Imagenes\llaves.ico')
 ventana.geometry('600x600')
 
-# Frames
-frame = Frame()
-frame.pack(fill='both')
-frame.config(bg='white')
-
 # Labels
-label = Label(frame, text='Ingrese la contraseña maestra', bg='white')
+label = Label(ventana, text='Ingrese la contraseña maestra', bg='white')
 label.config(height=1, width=25)
 label.pack()
 
 # Entry
-Entry = Entry(frame, bg='white')
+Entry = Entry(ventana, text='Hola Mundo', bg='white')
 Entry.pack()
 def get_text():
     text = Entry.get()
     p2.master_ppsw(text)
-    
 
 # Buttons
-button = Button(frame, text = 'Enviar', command='ejemplo')
+button = Button(ventana, text = 'Enviar', command='ejemplo')
 button.pack()
 
 
