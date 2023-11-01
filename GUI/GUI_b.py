6becc0587c0105642255fb08edff56992e7164b3
GeroDLuffy <gerox2009@hotmail.com>
2023-10-31 23:15:25 -0300

    Agregue pruebas

--- GUI/GUI_b.py
+++ GUI/GUI_b.py
@@ -1,20 +1,23 @@
 from tkinter import *
 ventana = Tk()
 
-screen_width = ventana.winfo_screenwidth()
-screen_height = ventana.winfo_screenheight()
+# screen_width = ventana.winfo_screenwidth()
+# screen_height = ventana.winfo_screenheight()
+ventana.geometry('1920x1080')
+
+# ventana.attributes('-fullscreen', True)
 
 # Frame Master Password
-frame_mp = Frame(ventana, bg='blue', width=screen_width, height=screen_height)
-frame_mp.pack(fill='both')
+# frame_mp = Frame(ventana, bg='blue', width=screen_width, height=screen_height)
+# frame_mp.pack(fill='both')
 
-label_mp = Label(frame_mp, text='Ingrese contraseña maestra: ')
-label_mp.grid(row=0, column=0, sticky='n', padx=5)
+# label_mp = Label(frame_mp, text='Ingrese contraseña maestra: ')
+# label_mp.grid(row=0, column=0, sticky='n', padx=5)
 
 
-entry_mp = Entry(frame_mp, bg='white')
-entry_mp.grid(row=0, column=1, sticky='n', padx=5)
-entry_mp.config(show='*', fg='black', justify='center')
+# entry_mp = Entry(frame_mp, bg='white')
+# entry_mp.grid(row=0, column=1, sticky='n', padx=5)
+# entry_mp.config(show='*', fg='black', justify='center')
 
 
 
