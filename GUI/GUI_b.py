51fa18e3daef4deba4516ed70550bf2f3c4e0998
GeroDLuffy <gerox2009@hotmail.com>
2023-11-01 12:00:08 -0300

    Pequeñas pruebas

--- GUI/GUI_b.py
+++ GUI/GUI_b.py
@@ -1,14 +1,33 @@
 from tkinter import *
 ventana = Tk()
 
-# screen_width = ventana.winfo_screenwidth()
-# screen_height = ventana.winfo_screenheight()
-ventana.geometry('1920x1080')
+screen_width = ventana.winfo_screenwidth()
+screen_height = ventana.winfo_screenheight()
+
 
 # ventana.attributes('-fullscreen', True)
 
+# ventana.geometry()
+
+# Ventana Master Password
+ventana.title('Password Manager')
+# ventana.geometry('600x600')
+# ventana.config()
+
+label_mp = Label(ventana, text='Ingrese contraseña maestra: ')
+label_mp.grid(row=0, column=0, sticky='n', padx=5)
+
+entry_mp = Entry(ventana, bg='white')
+entry_mp.config(show='*', justify='center')
+entry_mp.grid(row=1, column=0, sticky='n', padx=5)
+
+button_mp_q = Button(ventana, text='Salir', command=ventana.destroy).grid(row=2, column=0)
+
+button_mp_e = Button(ventana, text='Acceder').grid(row=2, column=1)
+
 # Frame Master Password
-# frame_mp = Frame(ventana, bg='blue', width=screen_width, height=screen_height)
+# frame_mp = Frame(ventana, bg='blue')
+# # frame_mp.grid()
 # frame_mp.pack(fill='both')
 
 # label_mp = Label(frame_mp, text='Ingrese contraseña maestra: ')
@@ -18,6 +37,8 @@ ventana.geometry('1920x1080')
 # entry_mp = Entry(frame_mp, bg='white')
 # entry_mp.grid(row=0, column=1, sticky='n', padx=5)
 # entry_mp.config(show='*', fg='black', justify='center')
+
+# button_mp = Button(frame_mp, text='Salir', command=ventana.destroy).grid(row=1, column=1)
 
 
 
